e47f1c4e0e48ac79704e0385cc8ef6425be50268
Hibikine Kage <goodsmile314159@gmail.com>
2017-01-16 07:53:55 +0900

    edited dein

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -2,3 +2,18 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dein.toml ~/.vim/rc
 ln -sf ~/dotfiles/dein_lazy.toml ~/.vim/rc
+if [ "$(uname)" == 'Darwin' ]; then
+    OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ] || [ "$(expr substr $(uname -s) 1 9)" == 'CYGWIN_NT' ]; then
+    OS='Cygwin'
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
+if [ $OS == 'Cygwin' ]; then
+    echo "Your platform is Cygwin."
+    ln -sf '~/dotfiles/Vivaldi/User Data/Default' '~/AppData/Local/Vivaldi/User Data'
+fi
