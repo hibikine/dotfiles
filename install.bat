8a43aa30c520b26c9d42d22bcb7d63de779990ba
BuildTools <unconfigured@null.spigotmc.org>
2017-11-27 02:00:47 +0900

    vim-syntasticになおした

--- install.bat
+++ install.bat
@@ -1,21 +1,2 @@
-@echo off
-for /d %%i in (.*) do (
-    if not %%i == ".git" (
-        echo %%i
-        mklink /d %%i "%userprofile%\%%i"
-    )
-)
-for %%i in (.*) do (
-    set TRUE_FALSE=TRUE
-    if %%i == ".gitignore" (
-        set TRUE_FALSE=FALSE
-    )
-    if %TRUE_FALSE% == TRUE (
-        echo %%i
-        mklink %%i "%userprofile%\%%i"
-    )
-    if %%i == ".vimrc" (
-        mklink %%i "%userprofile%\_vimrc"
-    )
-)
-pause
+mklink %userprofile%\.vimrc %userprofile%\dotfiles\.vimrc
+mklink /d %userprofile%\.vim %userprofile%\dotfiles\.vim
