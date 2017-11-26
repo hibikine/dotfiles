c299c1b6a1398c9d6d0a62920562c7456dc8865c
BuildTools <unconfigured@null.spigotmc.org>
2017-11-26 21:00:33 +0900

    installのコマンドを変更 ファイルの種類ごとに違うプラグインを読み込むようにした 画面切り替えのショートカットを追加

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,19 +1,8 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-mkdir -v -p ~/.vim/rc
-ln -sf ~/dotfiles/dein.toml ~/.vim/rc
-ln -sf ~/dotfiles/dein_lazy.toml ~/.vim/rc
-if [ "$(uname)" == 'Darwin' ]; then
-    OS='Mac'
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    OS='Linux'
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ] || [ "$(expr substr $(uname -s) 1 9)" == 'CYGWIN_NT' ]; then
-    OS='Cygwin'
-else
-    echo "Your platform ($(uname -a)) is not supported."
-    exit 1
-fi
+#!/bin/zsh
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
 
-if [ $OS == 'Cygwin' ]; then
-    echo "Your platform is Cygwin."
-fi
+    ln -sf ~/dotfiles/$f ~/$f
+done
