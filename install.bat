01748995945b3603ece5aeccce66dbc0508c4a5d
Hibikine Kage <goodsmile314159@gmail.com>
2018-02-02 12:45:57 +0900

    vscodeのWindows用インストールスクリプトを書いた

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
 mklink %userprofile%\.vimrc %userprofile%\dotfiles\.vimrc
 mklink /d %userprofile%\.vim %userprofile%\dotfiles\.vim
 mklink %userprofile%\.hyper.js %userprofile%\dotfiles\.hyper.js
+mklink %appdata%\Code\User\settings.json %userprofile%\dotfiles\vscode\settings.json
+
