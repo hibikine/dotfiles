0c263af0d055f0f9530cb4ae2888e7477a9dffc6
Kansei <goodsmile314159@gmail.com>
2018-02-07 19:01:09 +0900

    dockerの設定を.dotconfigに書き込むようにした

--- init_bow.sh
+++ init_bow.sh
@@ -5,9 +5,14 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update
 sudo apt-get install docker-ce -y
+touch .dotconfig
+echo "export DOCKER_TLS_VERIFY=
+DOCKER_HOST=
+DOCKER_CERT_PATH=
+DOCKER_MACHINE_NAME=" >> .dotconfig
 
 # Set default shell from bash to zsh
 sudo mv /bin/bash /bin/bashbin && \
@@ -18,4 +23,6 @@ if [ $SHELL = /bin/bash ]; then
 else
   /bin/bashbin $*
 fi' > /bin/bash
+sudo chmod 755 /bin/bash
+
 
