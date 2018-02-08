7d5f27c653934b9e4bc19b83038d8397100cca2d
Hibikine Kage <goodsmile314159@gmail.com>
2018-02-08 21:55:07 +0900

    vscodeの設定ファイルを分けた

--- install.bat
+++ install.bat
@@ -1,5 +1,9 @@
+del /Q %userprofile%\.vimrc
 mklink %userprofile%\.vimrc %userprofile%\dotfiles\.vimrc
+del /Q userprofile%\.vim 
 mklink /d %userprofile%\.vim %userprofile%\dotfiles\.vim
+del /Q %userprofile%\.hyper.js 
 mklink %userprofile%\.hyper.js %userprofile%\dotfiles\.hyper.js
-mklink %appdata%\Code\User\settings.json %userprofile%\dotfiles\vscode\settings.json
+del /Q %appdata%\Code\User\settings.json 
+mklink %appdata%\Code\User\settings.json %userprofile%\dotfiles\vscode\ms-settings.json
 
