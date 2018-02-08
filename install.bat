4eb3fa937e4143e4903de64b02931ee6d633b115
Hibikine Kage <goodsmile314159@gmail.com>
2018-02-08 22:11:56 +0900

    fix(install.bat): filename mistakes

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 mklink %userprofile%\.vimrc %userprofile%\dotfiles\.vimrc
 mklink /d %userprofile%\.vim %userprofile%\dotfiles\.vim
 mklink %userprofile%\.hyper.js %userprofile%\dotfiles\.hyper.js
-mklink %appdata%\Code\User\settings.json %userprofile%\dotfiles\vscode\settings.json
+mklink %appdata%\Code\User\settings.json %userprofile%\dotfiles\vscode\ms-settings.json
 
