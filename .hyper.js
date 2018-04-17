ba8b7dd8b2638a00ed476898b10b5ad8f9bb9e32
Hibikine Kage <goodsmile314159@gmail.com>
2018-04-18 01:50:34 +0900

    scheme変更

--- .hyper.js
+++ .hyper.js
@@ -120,7 +120,9 @@ module.exports = {
   // a list of plugins to fetch and install from npm format:
   // [@org/]project[#version] examples:   `hyperpower`   `@company/project`
   // `project#1.0.1`
-  plugins: ['hyperline'],
+  plugins: [
+    'hyperline', 'hyper-ayu'
+  ],
 
   // in development, you can create a directory under `~/.hyper_plugins/local/`
   // and include it here to load it and avoid it being `npm install`ed
