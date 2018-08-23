721200df7cfe69a9fa823e313cfed9ab19a09f40
Hibikine Kage <goodsmile314159@gmail.com>
2018-08-24 00:06:00 +0900

    fix: neovim install script

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -9,15 +9,15 @@ else
         ubuntu|debian)
             # Add vim repository
             sudo -E apt-get update && \
-                sudo -E apt-get install -y software-properties-common \
+                sudo -E apt-get install -y software-properties-common && \
                 sudo -E add-apt-repository ppa:neovim-ppa/unstable && \
                 sudo -E apt-get update && \
-                sudo -E apt-get install -y neovim python-neovim python3-neovim
-                sudo -E update-alternatives --install /usr/bin/vi vi /use/bin/nvim 60 \
-                sudo -E update-alternatives --config vi \
-                sudo -E update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60 \
-                sudo -E update-alternatives --config vim \
-                sudo -E update-alternatives --instlal /usr/bin/editor editor /usr/bin/nvim 60 \
+                sudo -E apt-get install -y neovim python-neovim python3-neovim && \
+                sudo -E update-alternatives --install /usr/bin/vi vi /use/bin/nvim 60 && \
+                sudo -E update-alternatives --config vi && \
+                sudo -E update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60 && \
+                sudo -E update-alternatives --config vim && \
+                sudo -E update-alternatives --instlal /usr/bin/editor editor /usr/bin/nvim 60 && \
                 sudo -E update-alternatives --config editor
             ;;
         osx)
