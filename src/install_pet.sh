1c449953f5ce26aead52d4679a039a3db091676b
Hibikine Kage <goodsmile314159@gmail.com>
2018-08-27 11:06:26 +0900

    fix: pet install

--- src/install_pet.sh
+++ src/install_pet.sh
@@ -2,7 +2,13 @@
 declare -a info=($(./get_os_info.sh))
 
 case ${info[0]} in
-    ubuntu | debian | osx)
+    ubuntu | debian)
+        mkdir -p $GOPATH/src/knqyf263
+        git clone https://github.com/knqyf263/pet $GOPATH/src/knqyf263/pet
+        cd $GOPATH/src/knqyf263/pet
+        make install
+        ;;
+    osx)
         brew install knqyf263/pet/pet
         ;;
 esac
