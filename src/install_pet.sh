35917aa2e7dc5902fdaa858a292bfa817788ad57
Hibikine Kage <goodsmile314159@gmail.com>
2019-03-29 03:44:07 +0900

    fix: pet

--- src/install_pet.sh
+++ src/install_pet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -a info=($(./get_os_info.sh))
-declare -a pet_version=($(./get_os_info.sh "knqyf263/pet"))
+declare -a pet_version=($(./get_latest_release.sh "knqyf263/pet"))
 declare -a pet_version_without_v=($(echo $pet_version | sed -e 's/v//'))
 
 case ${info[0]} in
