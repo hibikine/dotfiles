afaac4773100fbfe41dae2e6387584f31f6f85e7
Hibikine Kage <goodsmile314159@gmail.com>
2019-07-07 19:32:32 +0900

    fix: command

--- src/set_yarn_config.sh
+++ src/set_yarn_config.sh
@@ -2,8 +2,8 @@
 
 yarn config set init-license MIT -g
 yarn config set init-version 0.1.0 -g
-yarn config set '--emoji' true -g
-yarn config set '--network-timeout' 100000
-yarn config set yarn-offline-mirror "$HOME/.npm-packages-offline-cache"
-yarn-offline-mirror-pruning true
+yarn config set emoji true -g
+yarn config set network-timeout 100000 -g
+yarn config set yarn-offline-mirror "$HOME/.npm-packages-offline-cache" -g
+yarn config set yarn-offline-mirror-pruning true -g
 
