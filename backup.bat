38a1631f0bb0b5e28e66a371f16a5e0cfae8d3e2
Hibikine Kage <goodsmile314159@gmail.com>
2020-06-26 01:29:47 +0900

    add backup

---
+++ backup.bat
@@ -0,0 +1,5 @@
+xcopy E:\Pictures G:\Pictures /D /E /C /I /F /H /R /Y >> C:\log\%date:~0,4%_%date:~5,2%_%date:~8,2%_pictures_backup.log
+xcopy F:\MyMusics G:\MyMusics /D /E /C /I /F /H /R /Y >> C:\log\%date:~0,4%_%date:~5,2%_%date:~8,2%_mymusics_backup.log
+xcopy F:\MyCreated G:\MyCreated /D /E /C /I /F /H /R /Y >> C:\log\%date:~0,4%_%date:~5,2%_%date:~8,2%_mycreated_backup.log
+xcopy F:\illust G:\illust /D /E /C /I /F /H /R /Y >> C:\log\%date:~0,4%_%date:~5,2%_%date:~8,2%_illust_backup.log
+xcopy F:\sozai G:\sozai /D /E /C /I /F /H /R /Y >> C:\log\%date:~0,4%_%date:~5,2%_%date:~8,2%_sozai_backup.log
