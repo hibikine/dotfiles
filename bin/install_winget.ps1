cf37e0c465c6c0c14d2739eb704c546080d1eb66
Hibikine Kage <goodsmile314159@gmail.com>
2021-12-20 11:25:10 +0900

    add winget

---
+++ bin/install_winget.ps1
@@ -0,0 +1,22 @@
+winget install -i ImageLine.FLStudio
+winget install -i Git.Git -l "E:\Programs\Git"
+winget install Mozilla.Firefox
+winget install Google.Chrome
+winget install LINE.LINE
+winget install Telegram.TelegramDesktop
+winget install OpenJS.Nodejs
+winget install Discord.Discord
+winget install Adobe.AdobeAcrobatReaderDC
+winget install Ultimaker.Cura
+winget install Valve.Steam
+winget install Zoom.Zoom
+winget install KDE.Krita
+winget install UnityTechnologies.UnityHub
+winget install Logitech.LGH
+winget install Mojang.MinecraftLauncher
+winget install Twilio.Authy
+winget install OBSProject.OBSStudio
+winget install Audacity.Audacity
+winget install SlackTechnologies.Slack
+winget install Microsoft.PowerToys
+winget install 7zip.7zip
