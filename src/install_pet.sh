2ea995b46a4b97e29979dac3b26128abbd79614a
Hibikine Kage <goodsmile314159@gmail.com>
2022-11-13 11:48:15 +0900

    fix: 🐛 use apt for pet

--- src/install_pet.sh
+++ src/install_pet.sh
@@ -5,18 +5,7 @@ declare -a pet_version_without_v=($(echo $pet_version | sed -e 's/v//'))
 
 case ${info[0]} in
     ubuntu | debian)
-        case ${info[1]} in
-            i686)
-                curl -OL "https://github.com/knqyf263/pet/releases/download/${pet_version}/pet_${pet_version_without_v}_linux_386.deb" && \
-                    sudo -E dpkg --install "pet_${pet_version_without_v}_linux_386.deb" && \
-                    rm "pet_${pet_version_without_v}_linux_386.deb"
-                ;;
-            x86_64)
-                curl -OL "https://github.com/knqyf263/pet/releases/download/${pet_version}/pet_${pet_version_without_v}_linux_amd64.deb" && \
-                    sudo -E dpkg --install "pet_${pet_version_without_v}_linux_amd64.deb" && \
-                    rm "pet_${pet_version_without_v}_linux_amd64.deb"
-                ;;
-        esac
+        sudo -E apt install pet
         ;;
     osx)
         brew install knqyf263/pet/pet
