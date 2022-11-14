71215402cbd98483d61d07737a2488409d8911b4
Hibikine Kage <goodsmile314159@gmail.com>
2022-11-14 15:10:25 +0900

    fix: 🐛 pet installer
    on Ubuntu/Debianh, download the dev file from github release and install
    it

--- src/install_pet.sh
+++ src/install_pet.sh
@@ -2,11 +2,20 @@
 declare -a info=($(./get_os_info.sh))
 declare -a pet_version=($(./get_latest_release.sh "knqyf263/pet"))
 declare -a pet_version_without_v=($(echo $pet_version | sed -e 's/v//'))
+PET_DEB_PATH="./pet.deb"
 
 case ${info[0]} in
     ubuntu | debian)
-        sudo -E apt install pet
-        ;;
+        rm -f $PET_DEB_PATH
+        curl -s https://api.github.com/repos/knqyf263/pet/releases/latest \
+        | grep "browser_download_url.*amd64.deb" \
+        | cut -d : -f 2,3 \
+        | tr -d \" \
+        | wget -qi - -O $PET_DEB_PATH
+        # Install
+        sudo dpkg -i -GE $PET_DEB_PATH
+        # After Cleanup
+        rm -f $PET_DEB_PATH;;
     osx)
         brew install knqyf263/pet/pet
         ;;
