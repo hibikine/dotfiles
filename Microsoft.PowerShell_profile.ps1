b6288bcabfb083b6af94feb412f2041839ecb2bc
Hibikine Kage <goodsmile314159@gmail.com>
2024-03-29 12:01:16 +0900

    feat(profile.ps1): ✨ update profile.ps1
    - add checks to exnst the profile sub files
    - add alias to git status
    - add PSFzf, ZLocation
    - Add ssh-agent initializing scripts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,28 +1,35 @@
-fnm env --use-on-cd | Out-String | Invoke-Expression
-Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
-    . $_.FullName
+if (Get-Command fnm -ErrorAction SilentlyContinue) {
+    fnm env --use-on-cd | Out-String | Invoke-Expression
 }
-Get-ChildItem "$PROFILE\..\Local\" | ForEach-Object {
-    . $_.FullName
+if (Test-Path -Path "$PROFILE\..\Completions" -PathType Container) {
+    Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
+        . $_.FullName
+    }
+}
+if (Test-Path -Path "$PROFILE\..\Local\" -PathType Container) {
+    Get-ChildItem "$PROFILE\..\Local\" | ForEach-Object {
+        . $_.FullName
+    }
 }
 function gcom {
-  git commit -m $args
+    git commit -m $args
 }
 function gco {
-  git commit $args
+    git commit $args
 }
 
 function gc {
-  git checkout $args
+    git checkout $args
 }
 
 function gp {
-  git push $args
+    git push $args
 }
 
-function gs {
-  git status
+function Get-Git-Status {
+    git status
 }
+Set-Alias -Name gs -Value Get-Git-Status
 
 
 # Git Alias
@@ -37,5 +44,11 @@ Set-Alias -Name hyperlog -Value Git-Hyperlog
 function prompt { "`r`nPS " + $(Get-Location) + "`r`n> " }
 
 function youtube-dl-best() {
-  youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 $args[0]
+    youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 $args[0]
 }
+Import-Module PSFzf
+Enable-PsFzfAliases
+Import-Module ZLocation
+
+Start-Service ssh-agent
+ssh-add $env:USERPROFILE/.ssh/id_ed25519
